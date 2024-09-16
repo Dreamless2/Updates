4186d86741a8be4f6c7870c499f9c78ed8ece601
Tiago Pereira <tiagonet50@gmail.com>
2024-09-16 15:33:39 -0300

    Create teste.ps1

---
+++ teste.ps1
@@ -0,0 +1,13 @@
+$TempDir = "$env:TEMP"
+
+Start-BitsTransfer -Source "https://github.com/Dreamless2/Updates/blob/main/DS_PowerShell_Function_Library.psm1" -Destination $TempDir
+
+Import-Module "$TempDir\DS_PowerShell_Function_Library.psm1"
+
+DS_ClearPrefetchFolder
+
+$url = "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.4%2B7/OpenJDK21U-jdk_x64_windows_hotspot_21.0.4_7.msi"
+$filename = [System.IO.Path]::GetFileName($url)
+$filePath = Join-Path $TempDir $filename
+Start-BitsTransfer -Source $url -Destination $TempDir
+DS_InstallOrUninstallSoftware -File $filePath -Installationtype "Install" -Arguments "ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome"
